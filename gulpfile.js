56ae02431a044af2cf9db18d8924fd0c2a3ccd7c
Andrew Smirnov <sandark7@gmail.com>
2025-04-15 20:35:55 +0300

    third commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,13 @@
 var gulp = require('gulp'),
-  // jade = require('gulp-jade'),
   stylus = require('gulp-stylus'),
   watch = require('gulp-watch');
 var browserSync = require('browser-sync').create();
 
 var config = {
   stylus: "./src/styles/*.styl",
-  // jade:'./src/*.jade',
   html: "./*.html",
-  //video: "./*.mp4",
   dist: "./"
 };
-
-// gulp.task('video', function() {
-//     return gulp.src(config.video)
-//         .pipe(gulp.dest(config.dist))
-//         .pipe(browserSync.stream());
-// });
-
-// gulp.task('jade', function() {
-//   return gulp.src(config.jade)
-//     .pipe(jade({
-//       pretty: true
-//     }))
-//     .pipe(gulp.dest(config.dist))
-//     .pipe(browserSync.stream());
-// });
 
 gulp.task('stylus', function () {
   return gulp.src(config.stylus)
@@ -40,8 +22,6 @@ gulp.task('default', function () {
       baseDir: config.dist
     }
   });
-  // gulp.watch(config.jade, gulp.series('jade'));
   gulp.watch(config.stylus, gulp.series('stylus'));
   gulp.watch(config.html).on('change', browserSync.reload);
-  //gulp.watch(config.video, gulp.series('video'));
 });
